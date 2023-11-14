bbac0c6fa26d831a31169d5c70c6970773baa9e5
SiphiweMalete <150735986+SiphiweMalete@users.noreply.github.com>
2023-11-14 14:29:52 +0200

    Add files via upload

---
+++ port.js
@@ -0,0 +1,14 @@
+<script>
+        var tablinks = document.GetElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link")
+            }
+        for(tabcontent of tablinkcontents){
+                tabcontent.classList.remove("active-tab")
+        }
+        event.currentTarget.classList.add("active-link");
+
+    </script>
